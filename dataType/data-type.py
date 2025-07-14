ae2dd4dd4dad53066beb17ce0ccca75b59abe093
mustofa adny <adniadni04@gmail.com>
2025-07-14 20:39:12 +0700

    data type

---
+++ dataType/data-type.py
@@ -0,0 +1,21 @@
+# Integer
+print(20)
+
+# Float
+print(20.5)
+
+# Boolean
+print(True)
+
+# Complex
+print(5 + 7j)
+
+# List
+print([1, 2, 3, 4, 5])
+
+# Sets
+print({1, 2, 3, 4, 5})
+print(type({1, 2, 3, 4, 5}))
+
+# Dictionary
+print({"Name": "John", "Age": 30, "City": "New York"})
